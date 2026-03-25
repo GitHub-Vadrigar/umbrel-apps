c5454a812985364905216deaab8850c9046b4571
GitHub-Vadrigar <54599264+GitHub-Vadrigar@users.noreply.github.com>
2026-03-25 21:38:11 +0100

    Update and rename start.sh to start.sh

---
+++ nerva/scripts/start.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "Starting Nerva daemon..."
+
+/nerva/nervad \
+  --data-dir=/data/nerva \
+  --rpc-bind-ip=0.0.0.0 \
+  --rpc-bind-port=17566 \
+  --confirm-external-bind &
+
+echo "Waiting for nervad..."
+
+until curl -s http://localhost:17566/json_rpc \
+  -d '{"jsonrpc":"2.0","id":"0","method":"get_info"}' \
+  -H 'Content-Type: application/json' > /dev/null; do
+
+  sleep 2
+done
+
+echo "nervad ready"
+
+cd /app
+exec python3 server.py
